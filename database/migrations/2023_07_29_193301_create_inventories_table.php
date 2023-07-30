fb10dedd5e177fa0c08b5ed0a9c5ac2cad3943c6
zhangwuphp <zhang.wu@indzz.cn>
2023-07-30 23:04:42 +0800

    main:系统基本功能搭建

--- database/migrations/2023_07_29_193301_create_inventories_table.php
+++ database/migrations/2023_07_29_193301_create_inventories_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('material_id')->index();
+            $table->unsignedInteger('inbound_id')->nullable()->index();
+            $table->unsignedInteger('inbound_item_id')->nullable()->index();
+            $table->unsignedInteger('outbound_id')->index()->nullable();
+            $table->unsignedInteger('outbound_item_id')->index()->nullable();
+            $table->integer('qty')->default(0);
+            $table->decimal('unit_price',10,2)->default(0);
+            $table->timestamp('inbound_at')->nullable()->index();
+            $table->timestamp('outbound_at')->nullable()->index();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
